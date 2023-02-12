3de4a11dc4c1bd50ce21f45d82bc183911f8f59d
sankar <sankusanku001@gmail.com>
2023-02-12 10:02:23 +0530

    just started

---
+++ app/src/main/java/com/example/cricketmangemenet/JsonResponse.java
@@ -0,0 +1,7 @@
+package com.example.cricketmangemenet;
+
+import org.json.JSONObject;
+
+public interface JsonResponse {
+	public void response(JSONObject jo);
+}
---
+++ app/src/main/java/com/example/cricketmangemenet/ViewPlayers.java
@@ -0,0 +1,14 @@
+package com.example.cricketmangemenet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewPlayers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_players);
+    }
+}
